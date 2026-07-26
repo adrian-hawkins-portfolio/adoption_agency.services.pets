53030c26a271171d6c92348991058dc2e2ed93ab
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2026-07-26 15:38:02 +0200

    import

--- src/pet_service/main.py
+++ src/pet_service/main.py
@@ -1,5 +1,5 @@
 from adoption_agency_common import BOAFastApi
-from routers.pet_router import pet_router
+from pet_service.routers.pet_router import pet_router
 
 app = BOAFastApi()
 app.include_router(
